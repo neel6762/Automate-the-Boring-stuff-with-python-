1d8e37ecc72590b40a2533298e6acaff8f8bd82a
Nee Patel <38160394+neel6762@users.noreply.github.com>
2018-06-12 11:07:53 +0530

    Update regex_search.py

--- regex_search.py
+++ regex_search.py
@@ -1,46 +1,25 @@
 import re, os
 
+# set a location to search for
 path = r'C:\Files'
-listFiles = os.listdir(path)
-print('The files in the folder are \n', listFiles, "\n")
-container = []
+files = os.listdir(path)
 
-# iterate over the files in the "Files" folder and filter the .txt files
-for txtFile in listFiles:
-    match = re.compile(r'.*\.txt$')
-    test = (str(txtFile))
-    result = match.findall(test)
-    storeR = str(result).strip('[]')
-    container.append(storeR)
+# to filter the text files in the folder
+storeTxtFiles = []
+for txtFile in files:
+    if txtFile.endswith('.txt'):
+        storeTxtFiles.append(txtFile)
 
-# print the filtered list
-# print(container)
-
-# setting the path to open the file in the "Files" folder
-
-path = r'C:\Files'
-holder = []         # A empty list that will store the path file
-for files in container:
-    files = str(files).strip("''")
-    files = path + '\\' + files
-    # print(files)
-    holder.append(files)
-
-# print(holder)
-
-# take user input for searching
-searchFiles = input('Enter the word to search ....')
-print('\nSearching only in the text files...')
-
-for files in holder:
-   try:
-       file = open(files, 'r')
-       print('\nFile Opened:', files)
-       fileRead = file.read()
-       if searchFiles in fileRead:
-           print('Search Successful\nWord found in the file:',files)
-           break
-       print('Closing File:', files)
-       file.close()
-   except FileNotFoundError:
-       continue
+# get the user input to search the word in the files
+search = input('Enter the word to search in the text files..')
+val = 0
+for files in storeTxtFiles:
+    match = re.compile(search)
+    openedFile = open(path + '\\' + files)
+    fileData = openedFile.read()
+    if  match.search(fileData):
+        print('Search Successful' + '\nWord found in the file:' + files)
+        val = 1
+        break
+if val == 0:
+    print('Sorry the word was not found !')
